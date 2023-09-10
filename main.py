913f271b117e103a04ebdb014e67f35f891e81a4
johnjoemer <johnjoemerguevarra@gmail.com>
2023-09-10 16:15:09 +0800

    added open file dialog to select video and image

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ("hello world!")
+from tkinter import *
+from tkinter import filedialog
+
+def openFile():
+    filepath=filedialog.askopenfilename(title="Select Dataset",
+                                        filetypes=(("Images","*.jpg"),
+                                        ("Videos","*.mp4")))
+    file = open(filepath, 'r')
+    file.close()
+
+
+window = Tk()
+button = Button(text="Open", command=openFile)
+button.pack()
+window.mainloop()
+
