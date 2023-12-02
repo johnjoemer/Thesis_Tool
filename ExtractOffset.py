9c5750d1530abe031d5b603e0a19195d9a183c46
jamescenteno1124 <108730907+jamescenteno1124@users.noreply.github.com>
2023-12-03 01:12:49 +0800

    Add files via upload

---
+++ ExtractOffset.py
@@ -0,0 +1,38 @@
+import os
+import cv2
+import pandas as pd
+import shutil
+
+# Change csv_path, source_root_folder, output_folder values before running
+csv_path = r'C:\Users\Obey\OneDrive - Polytechnic University of the Philippines\Desktop\Thesis Tool\CodingForApexFrameExtraction.csv'
+df = pd.read_csv(csv_path)
+
+# Specify the source folder for images
+source_root_folder = r"C:\Users\Obey\Documents\CASME2_preprocessed_Li Xiaobai\CASME2_preprocessed_Li Xiaobai\Cropped"
+
+# Specify the output folder for copied images
+output_folder = r"C:\Users\Obey\OneDrive - Polytechnic University of the Philippines\Desktop\Thesis Tool\Dataset 6\Extracted_OffsetFrames"
+
+# Create the output folder if it does not exist
+os.makedirs(output_folder, exist_ok=True)
+
+# Iterates thru each row in the CSV
+for index, row in df.iterrows():
+    subject = str(row['Subject']).zfill(2)
+    filename = row['Filename']
+    offset_frame = row['OffsetFrame']
+
+    # Construct the source and destination file paths
+    source_path = os.path.join(source_root_folder, f'sub{subject}', filename, f'reg_img{offset_frame}.jpg')
+    output_path = os.path.join(output_folder, f'{filename}_frame{offset_frame}.jpg')
+
+    # Check if the source image file exists
+    if os.path.exists(source_path):
+        # Copy all images in the source folder to the output folder
+        for file_name in os.listdir(os.path.dirname(source_path)):
+            if file_name == f'reg_img{offset_frame}.jpg':
+                source_file_path = os.path.join(os.path.dirname(source_path), file_name)
+                output_file_path = os.path.join(output_folder, f'sub{subject}_{file_name}')
+                shutil.copy(source_file_path, output_file_path)
+    else:
+        print(f"Source image not found: {source_path}")
